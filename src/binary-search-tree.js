9a91b13e681066d0e90daa67e6b78e4a40f1c187
TrofimGest <gubaoslika@gmail.com>
2021-10-14 21:57:38 +0300

    feat: add new solutions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,119 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-module.exports = class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node{
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
 }
+module.exports = class BinarySearchTree {
+  constructor()
+    {
+        this.node = null;
+    }
+  
+    root() {
+      return this.node;
+    }
+  
+    add(data) {
+      var newNode = new Node(data);
+        if (this.node === null)
+        this.node = newNode;
+        else
+        this.addTreeNode(this.node, newNode)
+    }
+    addTreeNode(tree, newNode){
+      if (newNode.data < tree.data){
+        if (tree.left === null)
+          tree.left = newNode;
+        else this.addTreeNode(tree.left, newNode);
+      }
+      else{
+        if (tree.right === null)
+          tree.right = newNode;
+        else
+          this.addTreeNode(tree.right, newNode)
+      }
+    }
+  
+    has(data) {
+      if (this.find(data) !== null) return true
+      else return false
+    }
+  
+    find(data) {
+      return this.search(this.node, data);
+    }
+    search(tree, data){
+      if (tree === null)
+        return null;
+      else if(data < tree.data)
+        return this.search(tree.left, data);
+      else if(data > tree.data)
+        return this.search(tree.right, data);
+      else 
+        return tree;
+  
+    }
+  
+  
+    remove(data) {
+      this.node = this.removeNode(this.node, data);
+    }
+    removeNode(tree, data)
+    {
+      if (tree === null)
+        return null;
+    
+      else if (data > tree.data){
+        tree.right = this.removeNode(tree.right, data)
+        return tree
+      }
+      else if (data < tree.data){
+        tree.left = this.removeNode(tree.left, data)
+        return tree
+      }
+      else {
+        if(tree.left === null && tree.right === null){
+          tree = null;
+          return tree;
+        }
+        if (tree.left === null){
+          tree = tree.right;
+          return tree;
+        }
+        else if(tree.right === null){
+          tree = tree.left;
+          return tree;
+        }
+        var aux = this.findMin(tree.right);
+          tree.data = aux.data;
+   
+          tree.right = this.removeNode(tree.right, aux.data);
+          return tree;
+      }
+    }
+  
+    min() {
+      return this.findMin(this.node).data;
+    }
+    findMin(tree){
+      if (tree.left === null)
+        return tree
+      else
+        return this.findMin(tree.left);
+    }
+    max() {
+      return this.findMax(this.node).data;
+    }
+    findMax(tree){
+      if (tree.right === null)
+        return tree
+      else
+        return this.findMax(tree.right);
+    }
+  }
--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+module.exports = function removeKFromList(l, k) {
+  let current = l;
+  let nextNode = current.next;
+  while (l.value == k){
+    l = l.next
+  }
+  while(nextNode != null){
+    if (nextNode.value == k){
+     current.next = nextNode.next
+     if (current.next == null)
+     break;
+    }
+   current = current.next
+   nextNode = current.next
+  }
+  return l
+ }
